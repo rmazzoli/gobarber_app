1e40e8afb148161a6184aada6e3d150d1647d680
rmazzoli <rafael.mazzoli@hotmail.com>
2019-11-23 11:15:53 -0300

    Configurando Root Import

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+
+import Teste from '~/Teste';
 
 export default function App() {
-  return (
-    <View>
-      <Text>Hello World!</Text>
-    </View>
-  );
+  return <Teste />;
 }
